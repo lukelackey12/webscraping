5fef577e21499acf35192d748c0b62ae24de3c64
Luke <Luke_Lackey1@baylor.edu>
2024-04-16 19:40:59 -0500

    lets gooo

--- webscraping-COVID.py
+++ webscraping-COVID.py
@@ -14,15 +14,49 @@ from bs4 import BeautifulSoup
 ##  > sudo "./Install Certificates.command"
 
 
-
 url = 'https://www.worldometers.info/coronavirus/country/us'
 # Request in case 404 Forbidden error
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+req = Request(url, headers=headers)
 
+webpage = urlopen(req).read()
 
+soup = BeautifulSoup(webpage, 'html.parser')
 
+print(soup.title.text)
 
+table_rows = soup.findAll("tr")
 
+state_death_ratio = ""
+state_best_testing = ""
+state_worst_testing = ""
+highest_death_ratio = 0.0
+best_test_ratio = 0.0
+worst_test_ratio = 1000.0
+
+for row in table_rows[2:53]:
+    td = row.findAll("td")
+    state = td[1].text
+    total_cases = int(td[2].text.replace(",",""))
+    total_deaths = int(td[4].text.replace(",",""))
+    total_tested = int(td[10].text.replace(",",""))
+    population = int(td[12].text.replace(",",""))
+
+    death_ratio = total_deaths/total_cases
+    test_ratio = total_tested/population
+
+print("State with the highest death ratio is:", state_death_ratio)
+print(f"Death ratio: {highest_death_ratio:.2%}")
+print()
+print()
+print("State with the best testing ratio is:", state_best_testing)
+print(f"Test Ratio: {best_test_ratio:.2%}")
+print()
+print()
+print("State with the worst testing ratio is:", state_worst_testing)
+print(f"Test Ratio: {worst_test_ratio:.2%}")
+print()
+print()
 
 #SOME USEFUL FUNCTIONS IN BEAUTIFULSOUP
 #-----------------------------------------------#
